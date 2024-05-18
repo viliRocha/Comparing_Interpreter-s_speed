c6ea17048594adc5bc784cb4de42d0abf4dc3fac
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-05-18 17:09:53 -0300

    Update Python_code.py

--- Python_code.py
+++ Python_code.py
@@ -2,6 +2,8 @@ from timeit import timeit
 
 # Basic algorithm to put numbers from an array  in discending order
 
+code = ""
+
 numbers = [13, 5, 7, 200, 3, 7.9];
 
 def put_in_order():
@@ -15,5 +17,7 @@ def put_in_order():
 
       numbers[j + 1] = num
 
+algo_one_time = timeit(stmt=code, number=10000)
+
 put_in_order()
-print(numbers)
+print(numbers, f"Executed in: {algo_one_time}ms")
