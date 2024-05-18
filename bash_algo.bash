12bb25412b7d6136fb24482370793f085d5b3670
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-05-18 18:54:13 -0300

    Create bash_algo.bash

---
+++ bash_algo.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Unfortunatelly it is hard to support float numbers
+
+numbers=(13 5 7 200 3 9)
+
+put_in_order() {
+    for ((i=1; i<7; i++)); do
+        num=${numbers[i]}
+        j=$((i-1))
+        while ((j>=0 && numbers[j]>num)); do
+            numbers[j+1]=${numbers[j]}
+            j=$((j-1))
+        done
+        numbers[j+1]=$num
+    done
+}
+
+put_in_order
+echo "${numbers[@]}"
