4e38b0a33ed4e2dd53074d7cea39ae4c35d9aa89
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-05-18 20:20:40 -0300

    Create Algorithm_Ruby.rb

---
+++ Algorithm_Ruby.rb
@@ -0,0 +1,24 @@
+require 'benchmark'
+
+numbers = [13, 5, 7, 200, 3, 7.9]
+
+result = Benchmark.measure do
+    def put_in_order(numbers)
+    for i in 1...numbers.length
+        j = i - 1
+        num = numbers[i]
+    
+        while j >= 0 and numbers[j] > num
+        numbers[j + 1] = numbers[j]
+        j = j - 1
+        end
+    
+        numbers[j + 1] = num
+    end
+    end
+end
+
+put_in_order(numbers)
+print(numbers)
+
+puts " \n Execution time: #{result.real} milliseconds"
