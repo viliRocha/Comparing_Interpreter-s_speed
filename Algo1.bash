4813b9a2a601c04e06d548915359071ea35faf7f
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-08-29 22:14:17 -0300

    Now there are more examples

--- Algo1.bash
+++ Algo1.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Define the numbers as a space-separated string
-numbers="13, 5, 7, 200, 3, 7.9,"
+numbers="13 5 7 200 3 7.9"
 
 # Function to sort numbers in increasing order using awk
 put_in_order() {
-    # Use awk to sort the numbers
-    sorted_numbers=$(echo $numbers | awk '{for(i=1;i<=NF;i++) print $i}' | sort -n)
+    # Converting the array to a format suitable for awk
+    
+    # Using awk "sort" 
+    # sorted_numbers=$(echo $numbers | awk '{for(i=1;i<=NF;i++) print $i}' | sort -n)
+
+    # without awk sort (Adding + 0 in if (a[i] + 0 > a[j] + 0) forces awk to treat them as floats)
+    sorted_numbers=$(printf "%s\n" $numbers | awk '{a[NR] = $0} END {for (i = 1; i <= NR; i++) for (j = i + 1; j <= NR; j++) if (a[i] + 0 > a[j] + 0) {temp = a[i]; a[i] = a[j]; a[j] = temp} for (i = 1; i <= NR; i++) print a[i]}')
     # Convert the sorted output back into a variable
     numbers="$sorted_numbers"
 }
